4cfca6c9caa4ee7ae9ecc57bf3f2d140c8061630
JE <wjd7dms7@naver.com>
2022-10-03 23:12:29 +0900

    ~#4.0

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('hi')
+const loginForm = document.querySelector('#login-form')
+const loginInput = loginForm.querySelector('input')
+const loginButton = loginForm.querySelector('button')
+
+
+function onLoginBtnClick() {
+    console.dir(loginInput.value)
+}
+
+loginButton.addEventListener('click', onLoginBtnClick)
