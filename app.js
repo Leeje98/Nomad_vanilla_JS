4cacfe3b7eb26aeab7080da0b937a483e9b1d50c
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-04 17:58:52 +0900

    #3.4

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = loginForm.querySelector('input')
-const loginButton = loginForm.querySelector('button')
+const greeting = document.querySelector('#greeting')
 
 
-function onLoginBtnClick() {
-    console.dir(loginInput.value)
+
+function onLoginSubmit(event) {
+    event.preventDefault();  
+    const username = loginInput.value;
+    loginForm.classList.add('hidden')
+    console.log(username)
+    greeting.innerText = 'Hello' + username;
 }
 
-loginButton.addEventListener('click', onLoginBtnClick)
+
+loginForm.addEventListener('submit', onLoginSubmit)
