02a4a3c89d24b2719473127b3c48ff9a8026cfc3
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-05 17:58:15 +0900

    #7.6

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,18 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input')
 const toDoList = document.getElementById('todo-list');
 
-const toDos = [];
+const TODOS_KEY = 'todos'
+
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem('todos', toDos)
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 
 function deleteToDo(event) {
   const li = event.target.parentElement         // 어떤 li의 버튼을 클릭햇는지 감지
                   // target : 클릭된 엘리먼트
-                         // parentElement : 클릭된 엘리먼트의 부모
+                         // parentElement : 클릭된 엘리먼트의 부모 (li)
   li.remove()   // 클릭된 li 지우기
 }
 
@@ -31,12 +33,32 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
+  const newTodoObj = {
+    text:newTodo,
+    id:Date.now()
+  } /////////////4:07
   toDos.push(newTodo)
   paintToDo(newTodo)
   saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit)
+
+function sayHello(item) {
+  console.log('this is the turn of', item)
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  // parsedToDos.forEach((item) => console.log('this is the turn of', item));
+  // parsedToDos.forEach(sayHello);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+
 
 
 
