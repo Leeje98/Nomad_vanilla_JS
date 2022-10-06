d1b82a62b3a25f896a850407176e22fe72ca50ce
lee Jeong Eun <wjd7dms7@naver.com>
2022-10-06 16:29:35 +0900

    css 임시저장

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg']
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.png', '05.png', '06.png', '07.png', '08.png', '09.jpeg', '10.jpeg']
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
                 // 랜덤으로 배열의 인덱스 번호 뽑아내기
@@ -10,6 +10,9 @@ bgImage.src = `img/${chosenImage}`   // img src 생성
 
 document.body.appendChild(bgImage)
         // body의 자식으로 등록한다 : appendChild-가장 뒤에 / prependChild 가장 위에 삽입
+
+
+bgImage.classList.add('backgraundIMG')
 
 
 
